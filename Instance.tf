a14f321ccc6bc53926c31dc8579baad74ff79f07
salik1995 <129122912+salik1995@users.noreply.github.com>
2023-05-03 19:46:09 -0400

    Update Instance.tf

--- Instance.tf
+++ Instance.tf
@@ -2,12 +2,17 @@ provider "aws" {
   region = "us-east-1"
   access_key=var.AWS_ACCESS_KEY
   secret_key=var.AWS_SECRET_KEY
-}
+ }
 resource "aws_instance" "web" {
   ami         = "ami-0889a44b331db0194"
   instance_type = "t3.micro"
   
-  
+  }
+resource "aws_instance" "web" {
+  ami         = "ami-0889a44b331db0194"
+  instance_type = "t3.micro"
+  }
+
   tags = {
     Name = "HelloWorld"
     
