6d0fd5e7923a2129ab1fc05a5acc324fd7d8cb8d
salik1995 <129122912+salik1995@users.noreply.github.com>
2023-05-03 19:47:35 -0400

    Update Instance.tf

--- Instance.tf
+++ Instance.tf
@@ -3,12 +3,12 @@ provider "aws" {
   access_key=var.AWS_ACCESS_KEY
   secret_key=var.AWS_SECRET_KEY
  }
-resource "aws_instance" "web" {
+resource "aws_instance" "web1" {
   ami         = "ami-0889a44b331db0194"
   instance_type = "t3.micro"
   
   }
-resource "aws_instance" "web" {
+resource "aws_instance" "web2" {
   ami         = "ami-0889a44b331db0194"
   instance_type = "t3.micro"
   }
