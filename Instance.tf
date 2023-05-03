a9cc65a05c62810f2f52b41f7d1349355be07e1d
salik1995 <129122912+salik1995@users.noreply.github.com>
2023-05-03 19:39:15 -0400

    Update Instance.tf

--- Instance.tf
+++ Instance.tf
@@ -1,10 +1,10 @@
 provider "aws" {
-  region = "sa-east-1"
+  region = "us-east-1"
   access_key=var.AWS_ACCESS_KEY
   secret_key=var.AWS_SECRET_KEY
 }
 resource "aws_instance" "web" {
-  ami         = "ami-0da62eb5869c785b9"
+  ami         = "ami-0889a44b331db0194"
   instance_type = "t3.micro"
   
   
