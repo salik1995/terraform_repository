6e0cb042743538f5859abd1beedb7e0e1a69a7fe
salik1995 <129122912+salik1995@users.noreply.github.com>
2023-05-03 19:55:56 -0400

    Update Instance.tf

--- Instance.tf
+++ Instance.tf
@@ -7,6 +7,9 @@ resource "aws_instance" "web1" {
   ami         = "ami-0889a44b331db0194"
   instance_type = "t3.micro"
   
+  tags = {
+    Name = "HelloWorld"
+  }
   }
 resource "aws_instance" "web2" {
   ami         = "ami-0889a44b331db0194"
@@ -16,6 +19,6 @@ resource "aws_instance" "web2" {
   tags = {
     Name = "HelloWorld"
     
-  }
+  
 }
 }
